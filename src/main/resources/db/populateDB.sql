23238be228198d24a273bb262fd50c55a3730987
Petr Lukashev <lukashev_pjer@mail.ru>
2019-01-26 21:25:01 +0300

    changing Timestamp to Date in DB and adding enabled to dishes table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -2,6 +2,7 @@ DELETE FROM user_roles;
 DELETE FROM users;
 DELETE FROM dishes;
 DELETE FROM restaurants;
+DELETE FROM votes;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password ) VALUES
@@ -20,19 +21,19 @@ VALUES ('Metropol'),
        ('Koryushka'),
        ('MyLove');
 
-INSERT INTO dishes (name, price, restaurant_id) VALUES
+INSERT INTO dishes (name, price, restaurant_id, ENABLED) VALUES
 
-     ('Borsch', 80, 100002),
-     ('Solyanka', 120, 100003),
-     ('Pork', 160, 100004),
-     ('Roast', 170, 100005),
-     ('Eggs', 90, 100006),
-     ('Salad', 70, 100002),
-     ('Ice-cream', 60, 100004),
-     ('Chicken-soup', 90, 100003),
-     ('Salmon', 200, 100002),
-     ('Wok', 180, 100002),
-     ('Pizza', 150, 100002);
+     ('Borsch', 80, 100002, true),
+     ('Solyanka', 120, 100003, true),
+     ('Pork', 160, 100004, false),
+     ('Roast', 170, 100005, false),
+     ('Eggs', 90, 100006, true),
+     ('Salad', 70, 100002, false),
+     ('Ice-cream', 60, 100004, true),
+     ('Chicken-soup', 90, 100003, true),
+     ('Salmon', 200, 100002,true ),
+     ('Wok', 180, 100002, true ),
+     ('Pizza', 150, 100002,false);
 
 INSERT INTO votes(USER_ID, RESTAURANT_ID) VALUES
     (100000, 100002),
