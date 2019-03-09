7c973170b62aa66d014d15a4c8dca2b8e0f6db0f
Petr Lukashev <lukashev_pjer@mail.ru>
2019-03-09 16:34:51 +0300

    changing security classes

--- src/main/java/ru/lukashev/vote/web/LoggedUser.java
+++ src/main/java/ru/lukashev/vote/web/LoggedUser.java
@@ -1,86 +1,34 @@
 package ru.lukashev.vote.web;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.util.Assert;
-import ru.lukashev.vote.model.AbstractBaseEntity;
-import ru.lukashev.vote.model.Roles;
 import ru.lukashev.vote.model.User;
 import ru.lukashev.vote.to.UserTo;
 import ru.lukashev.vote.util.UserUtil;
 
-import java.util.Collections;
-import java.util.Objects;
-import java.util.Set;
+public class LoggedUser extends org.springframework.security.core.userdetails.User {
 
-public class LoggedUser implements UserDetails {
+    private static final long serialVersionUID = 1L;
 
     private UserTo userTo;
 
-protected int id = AbstractBaseEntity.START_SEQ;
+    public LoggedUser(User user) {
+        super(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, user.getRoles());
+        this.userTo = UserUtil.asTo(user);
+    }
 
-protected Set<Roles> roles = Collections.singleton(Roles.ROLE_USER);
+    public int getId() {
+        return userTo.getId();
+    }
 
-protected boolean enabled = true;
+    public void update(UserTo newTo) {
+        userTo = newTo;
+    }
 
-public LoggedUser(User u){
-    id = u.getId();
-roles=u.getRoles();
-enabled = u.isEnabled();
-this.userTo= UserUtil.asTo(u);
-}
-
-public static LoggedUser safeGet(){
-
-    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-if(auth==null){return null;}
-
-Object user = auth.getPrincipal();
-
-return (user instanceof LoggedUser) ? (LoggedUser) user: null;
-
-}
-
-//private static LoggedUser LOGGED_USER = new LoggedUser();
-
-public static LoggedUser get(){
-
-    LoggedUser loggedUser = safeGet();
-
-    Assert.notNull(loggedUser, "No authorized user found");
-    return loggedUser;
-}
-
-public static int id(){return get().userTo.getId();}
-
-public Set<Roles> getAuthorities(){return roles;}
-
-public boolean isEnabled(){return enabled;}
-
-    @Override
-    public String getPassword() {
-        return userTo.getPassword();
+    public UserTo getUserTo() {
+        return userTo;
     }
 
     @Override
-    public String getUsername() {
-        return userTo.getEmail();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return enabled;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return enabled;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return enabled;
+    public String toString() {
+        return userTo.toString();
     }
 }
--- src/main/java/ru/lukashev/vote/web/SecurityUtil.java
+++ src/main/java/ru/lukashev/vote/web/SecurityUtil.java
@@ -1,20 +1,32 @@
 package ru.lukashev.vote.web;
-import ru.lukashev.vote.model.AbstractNamedEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static java.util.Objects.requireNonNull;
 
 
 public class SecurityUtil {
 
-    private static int id = AbstractNamedEntity.START_SEQ;
+    public static LoggedUser safeGet() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            return null;
+        }
+        Object principal = auth.getPrincipal();
+        return (principal instanceof LoggedUser) ? (LoggedUser) principal : null;
+    }
+
+    public static LoggedUser get() {
+        LoggedUser user = safeGet();
+        requireNonNull(user, "No authorized user found");
+        return user;
+    }
 
     private SecurityUtil() {
     }
 
     public static int authUserId() {
-        return id;
-    }
-
-    public static void setAuthUserId(int id) {
-        SecurityUtil.id = id;
+        return get().getId();
     }
 
 }
